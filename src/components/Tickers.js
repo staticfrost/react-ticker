48f1a4ff3c7686f94d0b96a688a72680030c9ea3
staticfrost <scott@scottbailey.net>
2018-09-09 09:25:08 +1000

    added more coins and deployed

--- src/components/Tickers.js
+++ src/components/Tickers.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
-import axios from "axios";
-import "./Tickers.css";
-import Cryptocurrency from "./Cryptocurrency";
+import React, { Component } from 'react';
+import axios from 'axios';
+import './Tickers.css';
+import Cryptocurrency from './Cryptocurrency';
 
 class Tickers extends Component {
   constructor(props) {
@@ -9,31 +9,76 @@ class Tickers extends Component {
     this.state = {
       data: [
         {
-          id: "bitcoin",
-          name: "Bitcoin",
-          symbol: "BTC",
-          price_usd: "1",
-          percent_change_1h: "0",
-          percent_change_24h: "0",
-          percent_change_7d: "0"
+          id: 'bitcoin',
+          name: 'Bitcoin',
+          symbol: 'BTC',
+          price_usd: '1',
+          percent_change_1h: '0',
+          percent_change_24h: '0',
+          percent_change_7d: '0'
         },
         {
-          id: "ethereum",
-          name: "Ethereum",
-          symbol: "ETH",
-          price_usd: "1",
-          percent_change_1h: "0",
-          percent_change_24h: "0",
-          percent_change_7d: "0"
+          id: 'ethereum',
+          name: 'Ethereum',
+          symbol: 'ETH',
+          price_usd: '1',
+          percent_change_1h: '0',
+          percent_change_24h: '0',
+          percent_change_7d: '0'
         },
         {
-          id: "litecoin",
-          name: "Litecoin",
-          symbol: "LTC",
-          price_usd: "1",
-          percent_change_1h: "0",
-          percent_change_24h: "0",
-          percent_change_7d: "0"
+          id: 'litecoin',
+          name: 'Litecoin',
+          symbol: 'LTC',
+          price_usd: '1',
+          percent_change_1h: '0',
+          percent_change_24h: '0',
+          percent_change_7d: '0'
+        },
+        {
+          id: 'cardano',
+          name: 'Cardano',
+          symbol: 'ADA',
+          price_usd: '1',
+          percent_change_1h: '0',
+          percent_change_24h: '0',
+          percent_change_7d: '0'
+        },
+        {
+          id: 'binance-coin',
+          name: 'Binance Coin',
+          symbol: 'BNB',
+          price_usd: '1',
+          percent_change_1h: '0',
+          percent_change_24h: '0',
+          percent_change_7d: '0'
+        },
+        {
+          id: 'omisego',
+          name: 'OmiseGO',
+          symbol: 'OMG',
+          price_usd: '1',
+          percent_change_1h: '0',
+          percent_change_24h: '0',
+          percent_change_7d: '0'
+        },
+        {
+          id: 'stellar',
+          name: 'Stellar',
+          symbol: 'XML',
+          price_usd: '1',
+          percent_change_1h: '0',
+          percent_change_24h: '0',
+          percent_change_7d: '0'
+        },
+        {
+          id: 'icon',
+          name: 'Icon',
+          symbol: 'ICX',
+          price_usd: '1',
+          percent_change_1h: '0',
+          percent_change_24h: '0',
+          percent_change_7d: '0'
         }
       ]
     };
@@ -49,9 +94,18 @@ class Tickers extends Component {
 
   fetchCryptocurrencyData() {
     axios
-      .get("https://api.coinmarketcap.com/v1/ticker/")
+      .get('https://api.coinmarketcap.com/v1/ticker/')
       .then(response => {
-        var wanted = ["bitcoin", "ethereum", "litecoin"];
+        var wanted = [
+          'bitcoin',
+          'ethereum',
+          'litecoin',
+          'cardano',
+          'binance-coin',
+          'omisego',
+          'stellar',
+          'icon'
+        ];
         var result = response.data.filter(currency =>
           wanted.includes(currency.id)
         );
@@ -66,7 +120,12 @@ class Tickers extends Component {
     ));
     return (
       <div className="tickers-container">
-        <ul className="tickers">{tickers}</ul>
+        <p>
+          <ul className="tickers">{tickers.slice(0, 4)}</ul>
+        </p>
+        <p>
+          <ul className="tickers">{tickers.slice(4, 8)}</ul>
+        </p>
         <p>Information updated every minute courtesy of coinmarketcap.com</p>
       </div>
     );
